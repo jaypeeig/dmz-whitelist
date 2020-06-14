3122dc507bfe017a57caeeb2947d3ec21e9445f9
jaypeeig <jaypeeig@gmail.com>
2020-06-14 10:33:23 +0800

    Add url for generation

--- src/generate.js
+++ src/generate.js
@@ -1,6 +1,26 @@
+const qs = require('querystring');
+
 module.exports.handler = async event => {
-    return {
-        statusCode: 200,
-        body: JSON.stringify(event)
-    };
+    const response = {statusCode: 200};
+    try {
+        const payload = qs.decode(event.body);
+        response.body = JSON.stringify({
+            response_type: "ephemeral",
+            text: "Please click the link provided below.",
+            attachments:[
+                {
+                   "text":`Activation link: <${generate_url(event.body)}|allow-access-for-${payload.user_name}>`
+                }
+            ]
+        });
+    } catch (e) {
+       response.statusCode = 400;
+       response.body = e.message || 'Error in execution'
+    }
+    return response;
+};
+
+const generate_url = (payload) => {
+    const endpoint = process.env.API_URL;
+    return `${endpoint}connect?${payload}`;
 };

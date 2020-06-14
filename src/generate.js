f5d57b8775556efac6b451d3421b6a34ddc78ea9
jaypeeig <jaypeeig@gmail.com>
2020-06-14 10:42:05 +0800

    Add channel checker

--- src/generate.js
+++ src/generate.js
@@ -4,6 +4,12 @@ module.exports.handler = async event => {
     const response = {statusCode: 200};
     try {
         const payload = qs.decode(event.body);
+
+        if(payload.channel_name !== 'bastion'){
+            response.body = access_denied();
+            return response;
+        }
+
         response.body = JSON.stringify({
             response_type: "ephemeral",
             text: "Please click the link provided below.",
@@ -23,4 +29,11 @@ module.exports.handler = async event => {
 const generate_url = (payload) => {
     const endpoint = process.env.API_URL;
     return `${endpoint}connect?${payload}`;
+};
+
+const access_denied = () => {
+    return JSON.stringify({
+        response_type: "ephemeral",
+        text: "Bastion: Access Denied."
+    });
 };
